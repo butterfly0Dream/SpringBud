b9ab1e8c765bef24ac32f179a0a6364857d15a34
butterfly0Dream <525230813g@gmail.com>
2020-07-18 17:30:19 +0800

    1.日历demo 2.增加Glide依赖

--- JavaTest/src/main/java/com/fallgod/javatest/Test.java
+++ JavaTest/src/main/java/com/fallgod/javatest/Test.java
@@ -1,4 +1,7 @@
 package com.fallgod.javatest;
 
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Test");
+    }
 }
--- app/src/main/java/com/fallgod/springbud/ui/fragement/CalendarFragment.java
+++ app/src/main/java/com/fallgod/springbud/ui/fragement/CalendarFragment.java
@@ -1,10 +1,30 @@
 package com.fallgod.springbud.ui.fragement;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 import com.fallgod.springbud.BR;
 import com.fallgod.springbud.R;
 import com.fallgod.springbud.ui.base.BaseFragment;
 import com.fallgod.springbud.ui.base.DataBindingConfig;
+import com.fallgod.springbud.ui.calendar.Article;
+import com.fallgod.springbud.ui.calendar.ArticleAdapter;
+import com.fallgod.springbud.ui.calendar.GroupItemDecoration;
+import com.fallgod.springbud.ui.calendar.GroupRecyclerView;
 import com.fallgod.springbud.ui.viewmodel.CalendarViewModel;
+import com.fallgod.springbud.util.LogUtil;
+import com.haibin.calendarview.Calendar;
+import com.haibin.calendarview.CalendarLayout;
+import com.haibin.calendarview.CalendarView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 /**
  * Author: JackPan
@@ -12,9 +32,24 @@ import com.fallgod.springbud.ui.viewmodel.CalendarViewModel;
  * Time: 11:58
  * Description:
  */
-public class CalendarFragment extends BaseFragment {
+public class CalendarFragment extends BaseFragment implements
+        CalendarView.OnCalendarSelectListener,
+        CalendarView.OnYearChangeListener,
+        CalendarView.OnCalendarLongClickListener,
+        View.OnClickListener{
+    private static final String TAG = "CalendarFragment";
 
     private CalendarViewModel mCalendarViewModel;
+
+    TextView mTextMonthDay;
+    TextView mTextYear;
+    TextView mTextLunar;
+    TextView mTextCurrentDay;
+    CalendarView mCalendarView;
+    RelativeLayout mRelativeTool;
+    private int mYear;
+    CalendarLayout mCalendarLayout;
+    GroupRecyclerView mRecyclerView;
 
     @Override
     protected void initViewModel() {
@@ -25,6 +60,137 @@ public class CalendarFragment extends BaseFragment {
     protected DataBindingConfig getDataBindingConfig() {
         return new DataBindingConfig(R.layout.fragment_calendar,mCalendarViewModel)
                 .addBindingParams(BR.click,new ClickProxy());
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        initView();
+        initData();
+    }
+
+    @SuppressLint("SetTextI18n")
+    protected void initView() {
+        mTextMonthDay = getView().findViewById(R.id.tv_month_day);
+        mTextYear = getView().findViewById(R.id.tv_year);
+        mTextLunar = getView().findViewById(R.id.tv_lunar);
+        mRelativeTool = getView().findViewById(R.id.rl_tool);
+        mCalendarView = getView().findViewById(R.id.calendarView);
+        mTextCurrentDay = getView().findViewById(R.id.tv_current_day);
+        mTextMonthDay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mCalendarLayout.isExpand()) {
+                    mCalendarLayout.expand();
+                    return;
+                }
+                mCalendarView.showYearSelectLayout(mYear);
+                mTextLunar.setVisibility(View.GONE);
+                mTextYear.setVisibility(View.GONE);
+                mTextMonthDay.setText(String.valueOf(mYear));
+            }
+        });
+        getView().findViewById(R.id.fl_current).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCalendarView.scrollToCurrent();
+            }
+        });
+        mCalendarLayout = getView().findViewById(R.id.calendarLayout);
+        mCalendarView.setOnCalendarSelectListener(this);
+        mCalendarView.setOnCalendarLongClickListener(this);
+        mCalendarView.setOnYearChangeListener(this);
+        mTextYear.setText(String.valueOf(mCalendarView.getCurYear()));
+        mYear = mCalendarView.getCurYear();
+        mTextMonthDay.setText(mCalendarView.getCurMonth() + "月" + mCalendarView.getCurDay() + "日");
+        mTextLunar.setText("今日");
+        mTextCurrentDay.setText(String.valueOf(mCalendarView.getCurDay()));
+    }
+
+    protected void initData() {
+        int year = mCalendarView.getCurYear();
+        int month = mCalendarView.getCurMonth();
+
+        Map<String, Calendar> map = new HashMap<>();
+        map.put(getSchemeCalendar(year, month, 3, 0xFF40db25, "假").toString(),
+                getSchemeCalendar(year, month, 3, 0xFF40db25, "假"));
+        map.put(getSchemeCalendar(year, month, 6, 0xFFe69138, "事").toString(),
+                getSchemeCalendar(year, month, 6, 0xFFe69138, "事"));
+        map.put(getSchemeCalendar(year, month, 9, 0xFFdf1356, "议").toString(),
+                getSchemeCalendar(year, month, 9, 0xFFdf1356, "议"));
+        map.put(getSchemeCalendar(year, month, 13, 0xFFedc56d, "记").toString(),
+                getSchemeCalendar(year, month, 13, 0xFFedc56d, "记"));
+        map.put(getSchemeCalendar(year, month, 14, 0xFFedc56d, "记").toString(),
+                getSchemeCalendar(year, month, 14, 0xFFedc56d, "记"));
+        map.put(getSchemeCalendar(year, month, 15, 0xFFaacc44, "假").toString(),
+                getSchemeCalendar(year, month, 15, 0xFFaacc44, "假"));
+        map.put(getSchemeCalendar(year, month, 18, 0xFFbc13f0, "记").toString(),
+                getSchemeCalendar(year, month, 18, 0xFFbc13f0, "记"));
+        map.put(getSchemeCalendar(year, month, 25, 0xFF13acf0, "假").toString(),
+                getSchemeCalendar(year, month, 25, 0xFF13acf0, "假"));
+        map.put(getSchemeCalendar(year, month, 27, 0xFF13acf0, "多").toString(),
+                getSchemeCalendar(year, month, 27, 0xFF13acf0, "多"));
+        //此方法在巨大的数据量上不影响遍历性能，推荐使用
+        mCalendarView.setSchemeDate(map);
+
+
+        mRecyclerView = getView().findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.addItemDecoration(new GroupItemDecoration<String, Article>());
+        mRecyclerView.setAdapter(new ArticleAdapter(getActivity()));
+        mRecyclerView.notifyDataSetChanged();
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    private Calendar getSchemeCalendar(int year, int month, int day, int color, String text) {
+        Calendar calendar = new Calendar();
+        calendar.setYear(year);
+        calendar.setMonth(month);
+        calendar.setDay(day);
+        calendar.setSchemeColor(color);//如果单独标记颜色、则会使用这个颜色
+        calendar.setScheme(text);
+        calendar.addScheme(new Calendar.Scheme());
+        calendar.addScheme(0xFF008800, "假");
+        calendar.addScheme(0xFF008800, "节");
+        return calendar;
+    }
+
+
+    @Override
+    public void onCalendarOutOfRange(Calendar calendar) {
+        LogUtil.d(TAG,"onCalendarOutOfRange");
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onCalendarSelect(Calendar calendar, boolean isClick) {
+        mTextLunar.setVisibility(View.VISIBLE);
+        mTextYear.setVisibility(View.VISIBLE);
+        mTextMonthDay.setText(calendar.getMonth() + "月" + calendar.getDay() + "日");
+        mTextYear.setText(String.valueOf(calendar.getYear()));
+        mTextLunar.setText(calendar.getLunar());
+        mYear = calendar.getYear();
+    }
+
+    @Override
+    public void onYearChange(int year) {
+        mTextMonthDay.setText(String.valueOf(year));
+    }
+
+    @Override
+    public void onCalendarLongClickOutOfRange(Calendar calendar) {
+        LogUtil.d(TAG,"onCalendarLongClickOutOfRange");
+    }
+
+    @Override
+    public void onCalendarLongClick(Calendar calendar) {
+        LogUtil.d(TAG,"Calendar long click:"+calendar.getYear() + calendar.getMonth() + "月" + calendar.getDay() + "日");
     }
 
     public class ClickProxy{
